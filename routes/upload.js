5f5aaf24f23b6b41078bb2157861eba69b0bf214
Himanshu Sharma <himanshu.sharma.ext@heromotocorp.com>
2024-01-15 18:05:43 +0530

    uploading the current code

---
+++ routes/upload.js
@@ -0,0 +1,54 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').load();
+}
+
+const
+      express = require('express')
+    , router = express.Router()
+
+    , multer = require('multer')
+    , inMemoryStorage = multer.memoryStorage()
+    , uploadStrategy = multer({ storage: inMemoryStorage }).single('media')
+
+    , { BlockBlobClient } = require('@azure/storage-blob')
+    , getStream = require('into-stream')
+    , containerName = 'businessdata'
+;
+
+const handleError = (err, res) => {
+    res.status(500);
+    res.render('error', { error: err });
+};
+
+const getBlobName = originalName => {
+    const identifier = Math.random().toString().replace(/0\./, ''); // remove "0." from start of string
+    //return `${identifier}-${originalName}`;
+    return `${originalName}`;
+};
+
+router.post('/', uploadStrategy, (req, res) => {
+
+    const
+          blobName = getBlobName(req.file.originalname)
+        , blobService = new BlockBlobClient(process.env.AZURE_STORAGE_CONNECTION_STRING,containerName,blobName)
+        , stream = getStream(req.file.buffer)
+        , streamLength = req.file.buffer.length
+    ;
+
+    blobService.uploadStream(stream, streamLength)
+    .then(
+        ()=>{
+            res.render('success', { 
+                message: 'File uploaded to Azure Blob storage.' 
+            });
+        }
+    ).catch(
+        (err)=>{
+        if(err) {
+            handleError(err);
+            return;
+        }
+    })
+});
+
+module.exports = router;
